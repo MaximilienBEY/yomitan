077148e75f1d86f6201d55d25402c19ebadfcd87
Maximilien Bey <bey.maximilien@gmail.com>
2024-09-13 18:20:34 +0200

    continue to send visible & hidden event

--- ext/js/app/popup.js
+++ ext/js/app/popup.js
@@ -661,8 +661,10 @@ export class Popup extends EventDispatcher {
     _onVisibleChange({value}) {
         if (this._visibleValue === value) { return; }
 
-        this._visibleValue = value;
         const type = value ? 'visible' : 'hidden';
+        window.postMessage({source: 'YOMITAN', type}, '*');
+
+        this._visibleValue = value;
         this._frame.style.setProperty('visibility', type, 'important');
         void this._invokeSafe('displayVisibilityChanged', {value});
     }
